a68f72b3fb2a72a52f939c11816ca6c918d7e4de
Shashank Jain <sjain@optiproerp.com>
2018-07-31 14:48:46 +0530

    Added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftComponent } from './common/left/left.component';
+import { RightComponent } from './common/right/right.component';
+import { TopComponent } from './common/top/top.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent,
+    RightComponent,
+    TopComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
