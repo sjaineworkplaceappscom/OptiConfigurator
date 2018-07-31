8ff911c1af4668a613981bdf37b199e3ef221f80
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-07-31 15:25:27 +0530

    account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+
+
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown'; // Bootstrap Dropdown
+import { ModalModule } from 'ngx-bootstrap/modal'; // Bootstrap modal
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar'; // perfect scroll bar
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
+
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { GridModule, ExcelModule  } from '@progress/kendo-angular-grid';
+import { LayoutModule } from '@progress/kendo-angular-layout'
+import { PDFExportModule } from '@progress/kendo-angular-pdf-export'
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs'
+import { UploadModule } from '@progress/kendo-angular-upload'
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+
+// for svg icon
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +38,27 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ButtonsModule,
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    PerfectScrollbarModule,
+    HttpClientModule, 
+    AngularSvgIconModule, 
+    DropDownsModule, 
+    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    GridModule,
+    LayoutModule,
+    TabsModule.forRoot(),
+    PDFExportModule,
+    ExcelModule,
+    DateInputsModule,
+    UploadModule,
+    InputsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
