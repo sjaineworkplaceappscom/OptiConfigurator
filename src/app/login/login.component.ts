70b08acb83eb98bdc35b705510482bad3b44aac0
User1 <arjun@eamandloi@eworkplaceapps.com>
2018-07-31 16:11:14 +0530

    setup of login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userName: string;
+  password: string;
+ 
   constructor() { }
 
-  ngOnInit() {
-  }
+  @ViewChild('myCanvas') myCanvas;
 
+  ngOnInit() {
+    this.userName = '';
+    this.password = '';
+   
+    const element = document.getElementsByTagName("body")[0];
+    element.className = "";
+    element.classList.add("opti_body-login");
+    element.classList.add("opti_account-module");
+  }
 }
